8aa54ec24c40496d31d05f31370e66b1f3ec9690
3liNagi <37953000+3liNagi@users.noreply.github.com>
2018-04-28 17:43:52 +0200

    Beach Volley Ball
    Graphics Project

---
+++ script/playercontroller.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour {
+
+	public bool isGrounded;
+	public GameObject theball;
+	public AudioClip teleportsound;
+
+
+	private float speed;
+	private float w_speed = 0.05f;
+	public float rootspeed;
+	public float jumpheight;
+	Rigidbody rb;
+	Animator anim;
+	CapsuleCollider col_size;
+	public float turnSmoothTime = 0.2f;
+	float turnSmoothVelocity;
+
+
+
+ 
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		anim = GetComponent<Animator>();
+		col_size = GetComponent<CapsuleCollider>();
+		isGrounded = true;
+
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+	
+
+
+		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		Vector2 inputDir = input.normalized;
+
+		if (Input.GetKey (KeyCode.T)) {
+			
+			vollyball ball = theball.GetComponent ("vollyball")as vollyball;
+		   ball.Simulatprojectile();
+		
+		}
+
+
+
+		if (Input.GetKey(KeyCode.Space) && isGrounded == true)
+		{
+			rb.AddForce(0, jumpheight, 0);
+			anim.SetTrigger("isthrowball");
+			isGrounded = false;
+		}
+
+		if (Input.GetKey(KeyCode.X)  && isGrounded == true && transform.position.x - theball.transform.position.x <=5)
+		{
+			anim.SetTrigger("isthrowball");
+
+			vollyball ball =theball.GetComponent("vollyball")as vollyball;
+			//ball.ReleaseMe ();
+
+
+
+			/*if(teleportsound !=null)
+			{
+				AudioSource.PlayClipAtPoint (teleportsound , Camera.main.transform.position);
+
+			}*/
+
+		}
+
+		if(isGrounded)
+		{
+			speed = w_speed;
+
+			if (inputDir != Vector2.zero)
+			{
+				float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg;
+				transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+				anim.SetBool("iswalking", true);
+				anim.SetBool("isrunning", false);
+				anim.SetBool("isidel", false);
+			}
+
+			/*else if (Input.GetKey(KeyCode.LeftShift) )
+            {
+                anim.SetBool("iswalking", true);
+                anim.SetBool("isrunning", true);
+                anim.SetBool("isidel", false);
+            }*/
+			else
+			{
+				anim.SetBool("iswalking", false);
+				anim.SetBool("isrunning", false);
+				anim.SetBool("isidel", true);
+			}
+
+		}
+
+	}
+
+	void OnCollisionEnter()
+	{
+		isGrounded = true;
+
+	}
+
+
+}
+
+
+
+
+
+
---
+++ script/vollyball.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vollyball : MonoBehaviour {
+
+	public Transform target;
+	public float fireangle= 45.0f;
+	public float gravity = 9.8f;
+
+	public Transform projectile;
+	private Transform mytransform;
+
+
+	void Awake()
+	{
+		mytransform = transform;
+	}
+
+
+
+
+	public void Start () {
+		//StartCoroutine (Simulatprojectile());
+	}
+
+
+
+
+
+	void Update () {
+	}
+
+	public void Simulatprojectile()
+	{
+		
+
+		projectile.position = mytransform.position + new Vector3 (0, 0.0f, 0);
+
+
+		float target_distance = Vector3.Distance (projectile.position, target.position);
+
+		float projectile_velocity = target_distance / (Mathf.Sin (2 * fireangle * Mathf.Deg2Rad) / gravity);
+
+		float Vx = Mathf.Sqrt (projectile_velocity) * Mathf.Cos (fireangle * Mathf.Deg2Rad);
+			
+		float Vy = Mathf.Sqrt (projectile_velocity) * Mathf.Sin (fireangle * Mathf.Deg2Rad);
+
+		float flightduration = target_distance / Vx;
+
+		projectile.rotation = Quaternion.LookRotation (target.position - projectile.position);
+
+		float elapse_time = 0;
+
+		while(elapse_time < flightduration)
+		{
+			projectile.Translate (0, (Vy - (gravity * elapse_time)) * Time.deltaTime, Vx * Time.deltaTime);
+
+			elapse_time += Time.deltaTime;
+
+		}
+
+
+
+
+
+
+
+	}
+
+
+}
